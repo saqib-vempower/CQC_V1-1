894f40f5cb3c6b18d0ee03b112dafdcadfe87ec7
Ramraj Singh <ramraj@vempower.org>
2025-08-13 21:12:53 +0000

    Why send an email? Change that button to Send Login Link to Login.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -146,7 +146,7 @@ export default function LoginPage() {
                    />
                    <Button type="submit" className="w-full" disabled={isLoading}>
                        {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                     Send Login Link
+                     Login
                    </Button>
                  </form>
                </Form>
