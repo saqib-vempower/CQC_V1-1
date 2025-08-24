762627cd0ab4c3f3ecdc52e4c6afe67daed5ca9f
Ramraj Singh <ramraj@vempower.org>
2025-08-24 02:34:46 +0000

    Changes has been made, all the files are check. No update on color and style.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 // src/app/layout.tsx
-import './globals.css'; // not ../ or from a nested route
+import './globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body>{children}</body>
     </html>
   );
